46e66d1a97fb178c67d71b152ddbd5433fa078d6
Felipe Manuel Jacobs Osorio <felipejacobs28@gmail.com>
2018-08-04 19:14:16 +0200

    CausAhorro
    1.- Agregando Maven (thymeleaf, ldap, kafka, cassandra, jdbc)

---
+++ src/main/java/com/fvfintech/causahorro/CausahorroApplication.java
@@ -0,0 +1,12 @@
+package com.fvfintech.causahorro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CausahorroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CausahorroApplication.class, args);
+	}
+}
